4104dcafe87be3722ff751f983b170d024b82dd8
Brock Cooper <brock.cooper@snowflake.com>
2021-11-08 11:55:25 -0700

    remove original_text field

--- translate_from_english.py
+++ translate_from_english.py
@@ -22,7 +22,6 @@ def handler(event, context, local=False):
                     TargetLanguageCode=language)["TranslatedText"]
                 text_list.append({"language": language,
                                   "translated_text": translated_text,
-                                  "original_text": row[1],
                                   "status": 'success'})
             except Exception as e:
                 print(e)
